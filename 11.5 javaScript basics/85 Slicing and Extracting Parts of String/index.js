27fd3a62984ab66ec6cd4213bd5f739a7cc976bc
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-05-27 18:32:44 +0200

    JavaScript Basics changes

---
+++ 11.5 javaScript basics/85 Slicing and Extracting Parts of String/index.js
@@ -0,0 +1,6 @@
+let message = prompt("Wprowadż wiadomość (do 140 znaków):");
+var message140 = message.slice(0,140);
+alert(message140);
+
+
+alert(prompt("Wprowadź wiadomość do 140 znaków: ").slice(0, 140));
