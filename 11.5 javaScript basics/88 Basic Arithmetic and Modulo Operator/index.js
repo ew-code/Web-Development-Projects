23eabb4308f4d3e9c9920c5b7c5499c3a16344e3
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-05-28 10:22:55 +0200

    Basic Arithmetic and Modulo Operator added

---
+++ 11.5 javaScript basics/88 Basic Arithmetic and Modulo Operator/index.js
@@ -0,0 +1,7 @@
+// Dog Age to Human Age Formula
+// humanAge = (dogAge - 2) * 4 + 21
+
+var dogAge = prompt("How old is your dog? ");
+var humanAge = ((dogAge - 2) * 4) + 21;
+
+alert("Your dog Age is ," + humanAge + " years old in human years.");
