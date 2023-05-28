411b89df8eb5f330e66844673ccdc2309c63fb9e
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-05-28 19:20:17 +0200

    JavaScript Basics - parameters and arguments added

--- 11.5 javaScript basics/88 Basic Arithmetic and Modulo Operator/index.js
+++ 11.5 javaScript basics/88 Basic Arithmetic and Modulo Operator/index.js
@@ -5,3 +5,28 @@ var dogAge = prompt("How old is your dog? ");
 var humanAge = ((dogAge - 2) * 4) + 21;
 
 alert("Your dog Age is ," + humanAge + " years old in human years.");
+
+// modulo example:
+// var mod = 16 % 4; 16 / 4 - 4  = 0
+    // (reszta z dzielenia)
+
+    function getMilk() {   
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
---
+++ 11.5 javaScript basics/94 Parameters and Arguments/index.js
@@ -0,0 +1,47 @@
+function getMilk(money) {
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  var numberOfBottles = Math.floor(money / 1.5);
+  console.log("buy milk " + numberOfBottles  + " bottles of milk");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+
+getMilk(10);
+
+//
+// function getMilk(money) {
+  // console.log("leaveHouse");
+  // console.log("moveRight");
+  // console.log("moveRight");
+  // console.log("moveUp");
+  // console.log("moveUp");
+  // console.log("moveUp");
+  // console.log("moveUp");
+  // console.log("moveRight");
+  // console.log("moveRight");
+  // console.log("buy milk " + money/1.5  + " bottles of milk");
+  // console.log("moveLeft");
+  // console.log("moveDown");
+  // console.log("moveDown");
+  // console.log("moveDown");
+  // console.log("moveDown");
+  // console.log("moveLeft");
+  // console.log("moveLeft");
+  // console.log("enterHouse");
+// }
+//
+// getMilk(10);
