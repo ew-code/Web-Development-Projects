f68be80f12f4910de0e5803a1fe346187be7695a
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-05-28 09:32:01 +0200

    javaScript basics changes

--- 11.5 javaScript basics/86 Changing String Casing in Text/index.js
+++ 11.5 javaScript basics/86 Changing String Casing in Text/index.js
@@ -13,3 +13,30 @@ alert("Hello ," + nameStar + rest);
 // var nameSt = nameDo.slice(1,30);
 // var res = name2.slice(0,1);
 // alert("Hello ," + res + nameSt);
+
+//--------------------------------------------------------------------------
+//SOLUTION
+//1 Create a var that stores the name that user enters via prompt.
+var name = prompt("What is your name? ");
+
+//2 Capitalise the first letter of their name.
+
+// a isolate the first char
+var firstChar = name.slice(0, 1);
+
+//b Turn the first char to upper case.
+var upperCaseFirstChar = firstChar.toUpperCase();
+
+//c Isolate the  rest of the name
+var restOfName = name.slice(1, name.lenght);
+
+//d  Change the rest of the name to lower case.
+restOfName = restOfName.toLowerCase();
+
+//e concactenate the first char with the rest of the char
+var capitalisedName = upperCaseFirstChar + restOfName;
+
+//3 We use the capitalised version of their name to greet them using an alert.
+alert("Hello ," + capitalisedName);
+
+
