6c77f496f055c22ec035b53a8613a8124c324fea
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-05-29 18:51:16 +0200

    Coding Exercise added

---
+++ 11.5 javaScript basics/95 Coding Exercise/index.js
@@ -0,0 +1,15 @@
+function lifeInWeeks(age) {
+
+/************Don't change the code above************/
+
+    //Write your code here.
+    var yearsLeft = 90 - age;
+    var days = 365*yearsLeft;
+    var weeks = 52*yearsLeft;
+    var months = 12*yearsLeft;
+
+console.log("You have " + days + " days, "+ weeks + " weeks," + " and " + months + " months left." );
+
+/*************Don't change the code below**********/
+}
+
