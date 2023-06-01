42481142d627d04828a8480dab9d146772341da4
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-06-01 21:45:13 +0200

    Random Number Generator added

---
+++ 11.6 JavaScript Intermediate/100 Random Number Generator/index.js
@@ -0,0 +1,22 @@
+var n = Math.random();
+n = n * 6;
+n = Math.floor(n);
+console.log(n);
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+var n = Math.random();
+n = Math.floor(n * 6) + 1;
+console.log(n);
+
+
+var n = Math.random();
+n = Math.floor(n * 100) + 1;
+console.log(n);
+
+
+var firstName = prompt("Enter first name ");
+var secondName = prompt("Enter second name ");
+var n = Math.random();
+n = Math.floor(n * 100) + 1;
+alert(n + "%");
