70c3ecdb655330f2661f3969f9776736bb54f69f
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-06-03 15:44:06 +0200

    JavaScript Intermediate BMI Advanced added

--- 11.6 JavaScript Intermediate/104 BMI Calculator Challenge Advanced (IF/ELSE)/index.js
+++ 11.6 JavaScript Intermediate/104 BMI Calculator Challenge Advanced (IF/ELSE)/index.js
@@ -1,7 +1,16 @@
-function calcBmi(weight, height) {
-    var bmi = weight / Math.pow(height,2);
-    return Math.round(bmi);
-}
+function bmiCalculator(weight, height) {
+    var bmi = weight / Math.pow(height, 2);
 
-var bmi = calcBmi(65, 1.8);
-console.log("Your BMI is: " + bmi);
+    if (bmi < 18.5) {
+        return ("Your BMI is " + bmi + ", so you are underweight.");
+    }
+
+    if (bmi > 18.5 && bmi <= 24.9) {
+        return ("Your BMI is " + bmi + ", so you have a normal weight.");
+    }
+
+    if (bmi > 24.9) {
+        return ("Your BMI is " + bmi + ", so you are overweight.");
+
+    }
+}
