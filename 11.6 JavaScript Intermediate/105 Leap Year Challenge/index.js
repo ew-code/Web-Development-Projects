3c7ebf6f0d5d15ed61a21cb6744baed666dd9412
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-06-04 12:41:49 +0200

    Leap year chalenge added

--- 11.6 JavaScript Intermediate/105 Leap Year Challenge/index.js
+++ 11.6 JavaScript Intermediate/105 Leap Year Challenge/index.js
@@ -1,14 +1,33 @@
 function isLeap(year) {
 
-    if (year / 4 && year % 100) {
-        return ("Leap year.");
-    }
-
-    if (year % 4 && year / 100 && year % 400) {
-        return ("Not leap year.");
-    }
-
-    if (year / 400) {
-        return ("Leap year.");
+    if (year % 4 === 0) {
+        if (year % 100 == 0) {
+            if (year % 400 === 0) {
+                return "Leap year.";
+            } else {
+                return "Not leap year."
+            }
+        } else {
+            return "Leap year.";
+        }
+    } else {
+        return "Not leap year.;"
     }
 }
+
+// 
+// function isLeap(year) {
+// 
+    // if (year / 4 && year % 100) {
+        // return ("Leap year.");
+    // }
+// 
+    // if (year % 4 && year / 100 && year % 400) {
+        // return ("Not leap year.");
+    // }
+// 
+    // if (year / 400) {
+        // return ("Leap year.");
+    // }
+// }
+// 
