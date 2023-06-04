1b834aa6bf6c315b1e780167eba912423471cd4e
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-06-04 21:36:54 +0200

    Random Name fnction added

---
+++ 11.6 JavaScript Intermediate/107 Code Challenge - Who's Buying Lunch/index.js
@@ -0,0 +1,27 @@
+function getRandomName(names) {
+    const randomIndex = Math.floor(Math.random() * names.length);
+    return names[randomIndex];
+}
+
+Example usage
+const nameList = ['Alice', 'Bob', 'Charlie', 'David', 'Eve'];
+const randomName = getRandomName(nameList);
+console.log(randomName);
+
+
+
+// const randomIndex = Math.floor(Math.random() * names.length);
+// return names[randomIndex] + " is going to buy lunch today!";
+
+// Kolejność if else ma znaczenie (działa jak przepływ wody)!
+
+// programiści używają modulo % żeby sprawdzić czy dana liczba jest w całości podzielna przez inną liczbę (nie pozostawia reszty z dzielenia)
+
+// console.log(guestList.length);
+// guestList[0];
+
+var output = [];
+output.push(1); /zawsze dodaje element na końcu array
+output.pop; (zabierze ostatni element i go usunie)
+
+
