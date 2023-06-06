375f595c9fd5c599a641235630d873268d9345fe
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-06-06 19:35:25 +0200

    While loops challenge changes

--- 11.6 JavaScript Intermediate/110 Control Statements While Loops Challenge/index.js
+++ 11.6 JavaScript Intermediate/110 Control Statements While Loops Challenge/index.js
@@ -5,41 +5,23 @@ while (i < 2) {
     i++;
 }
 
-"99 bottles of beer on the wall, 99 bottles of beer.Take one down and pass it around, 98 bottles of beer on the wall."
 
-"1 bottle of beer on the wall,1 bottle of beer.Take one down and pass it around, no more bottles of beer on the wall."
-
-"No more bottles of beer on the wall, no more bottles of beer.Go to th store and buy some more, 99 bottles of beer on the wall."
-
-var output = [];
-var beer = 99;
+var bottle = 99;
 function beer() {
 
-    while (beer >= 1) {
+    while (bottle >= 1 ) {
 
-        if (beer === 99) {
-            console.log(output);
+        if (bottle <= 99) {
+            console.log(bottle + " bottles of beer on the wall, 99 bottles of beer.Take one down and pass it around, " + bottle + " bottles of beer on the wall.");
         }
 
-        if (beer === 1) {
-            console.log(output);
+        if (bottle === 1) {
+            console.log(bottle + " bottle of beer on the wall,1 bottle of beer.Take one down and pass it around, no more bottles of beer on the wall.");
         }
 
-        beer--;
-
-    }
-        console.log(output);
+        bottle--;
     }
 
-// Kolejność if else ma znaczenie (działa jak przepływ wody)!
+    console.log("No more bottles of beer on the wall, no more bottles of beer.Go to th store and buy some more, 99 bottles of beer on the wall.");
 
-// programiści używają modulo % żeby sprawdzić czy dana liczba jest w całości podzielna przez inną liczbę (nie pozostawia reszty z dzielenia)
-
-// console.log(guestList.length);
-// guestList[0];
-
-var output = [];
-output.push(1); /zawsze dodaje element na końcu array
-output.pop; (zabierze ostatni element i go usunie)
-
-
+    }
