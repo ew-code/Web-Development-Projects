91e1ced78504308e38220210c0d8081b775f6f09
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-06-10 12:50:53 +0200

    Changing Styles of HTML elements in Javascript

---
+++ 11.7 Document Object Model (DOM)/119 Manipulating and Changing Styles of HTML Elements with JavaSript/index.js
@@ -0,0 +1,7 @@
+
+
+document.querySelector("li a").style.color = "red";
+document.querySelector("h1").style.fontSize = "10rem"; //wartość do zmiany podajemy w stringu
+
+
+document.querySelector("button").style.backgroundColor = "yellow";
