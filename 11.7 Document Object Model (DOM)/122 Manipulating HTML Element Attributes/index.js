5d071cd292b5fecffdf348212cbc01b79e34dbba
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-06-10 16:37:32 +0200

    Manipulating HTML Elements Attributes

---
+++ 11.7 Document Object Model (DOM)/122 Manipulating HTML Element Attributes/index.js
@@ -0,0 +1,7 @@
+// atrybutami w htmlu są wszytkie elementy pomarańczowe (np.klasa, href, source) wszytko co jest w  srodku tagu jest atrybutem
+
+document.querySelector("a").attributes;
+
+document.querySelector("a").getAttribute("href");
+
+document.querySelector("a").setAttribute("href", "https:www.bing.com");
