39e5e37a2e8a28b2dfc4900a82b655ce5f112f65
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-06-10 16:15:40 +0200

    Text manipulation added

---
+++ 11.7 Document Object Model (DOM)/121 Text Manipulation and the Text Content Property/index.js
@@ -0,0 +1,3 @@
+document.getElementById("title").innerHTML = "<em>Good bye</em>";
+
+document.getElementById("title").textContent = "Good morning";//zwraca tylko teść tekstu bez dodatkowych elementów
