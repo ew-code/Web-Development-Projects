fabbaa91d6333df47de66e4add6643a410ad0177
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-06-10 11:57:03 +0200

    Selecting HTML elements with Jvascript

--- 11.7 Document Object Model (DOM)/118 Selecting HTML Elements with Javascript/index.js
+++ 11.7 Document Object Model (DOM)/118 Selecting HTML Elements with Javascript/index.js
@@ -1,3 +1,30 @@
 
 
-document.getElementsByTagName("li") // złapie wszytkie elementy listy, a nie tylko jeden
+document.getElementsByTagName("li") // złapie wszytkie elementy listy, a nie tylko jeden.Dlatego należy wskazać który element chcemy zmienić ->
+
+document.getElementsByTagName("li")[2].style.color = "purple";
+
+document.getElementsByTagName("li").length;
+
+
+
+document.getElementsByClassName("btn")[0].style.color = "red";
+
+
+
+document.getElementById("title").innerHTML = "Good Bye";
+
+
+// pozwala na więcej , i najczęściej użwany
+document.querySelector("h1");
+document.querySelector("#title");
+document.querySelector(".btn");
+document.querySelector("li a");
+document.querySelector("li.item");
+document.querySelector("#list a");
+document.querySelector("#list .item"); //zwróci tylko pierwszy element z listy
+document.querySelectorAll("#list .item") //zwróci wszytkie elementy z listy
+document.querySelectorAll("#list .item")[2];
+
+
+document.querySelector("li a").style.color = "red";
