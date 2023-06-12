1e39f8fc2920c6631bb970915df104ef4f1dcc6d
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-06-12 19:47:07 +0200

    Drum Kit changes in js

--- 18 Advanced Javascript and DOM Manipulation/Drum Kit Project/index.js
+++ 18 Advanced Javascript and DOM Manipulation/Drum Kit Project/index.js
@@ -1,10 +1,8 @@
-document.querySelectorAll("button")[0].addEventListener("click", handleClick);
-document.querySelectorAll("button")[1].addEventListener("click", handleClick);
-document.querySelectorAll("button")[2].addEventListener("click", handleClick);
-document.querySelectorAll("button")[3].addEventListener("click", handleClick);
-document.querySelectorAll("button")[4].addEventListener("click", handleClick);
-document.querySelectorAll("button")[5].addEventListener("click", handleClick);
-document.querySelectorAll("button")[6].addEventListener("click", handleClick);
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+}
 
 function handleClick() {
     alert("I got clicked!");
