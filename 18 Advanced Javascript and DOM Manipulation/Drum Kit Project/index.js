e64193ec66699e57321d5a1183036fe95c3f691c
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-06-12 19:35:36 +0200

    Drum Kit Project

---
+++ 18 Advanced Javascript and DOM Manipulation/Drum Kit Project/index.js
@@ -0,0 +1,11 @@
+document.querySelectorAll("button")[0].addEventListener("click", handleClick);
+document.querySelectorAll("button")[1].addEventListener("click", handleClick);
+document.querySelectorAll("button")[2].addEventListener("click", handleClick);
+document.querySelectorAll("button")[3].addEventListener("click", handleClick);
+document.querySelectorAll("button")[4].addEventListener("click", handleClick);
+document.querySelectorAll("button")[5].addEventListener("click", handleClick);
+document.querySelectorAll("button")[6].addEventListener("click", handleClick);
+
+function handleClick() {
+    alert("I got clicked!");
+}
