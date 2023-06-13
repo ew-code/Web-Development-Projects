2c5a052e5bd238c2c3fc4fba95b97c69722b55bd
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-06-13 19:56:36 +0200

    Drum Kit Project changes

--- 18 Advanced Javascript and DOM Manipulation/Drum Kit Project/index.js
+++ 18 Advanced Javascript and DOM Manipulation/Drum Kit Project/index.js
@@ -1,9 +1,64 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+    document.querySelectorAll(".drum")[i].addEventListener("click", playDrum);
 }
 
-function handleClick() {
-    alert("I got clicked!");
+function playDrum() {
+    console.lof(this.innerHTML);
 }
+
+
+
+// function playDrum() {
+    // var audio = new Audio('./sounds/tom-1.mp3');
+    // audio.play();
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// --------------------------------------------------------------------------------------
+// Higher Order Functions (are functions that can take other functions as inputs)
+
+// function add(num1, num2) {
+    // return num1 + num2;
+    // }
+
+// function multiply(num1, num2) {
+    // return num1 * num2;
+// }
+
+// function subtract(num1, num2) {
+    // return num1 - num2;
+// }
+
+// function divide(num1, num2) {
+    // return num1 / num2;
+// }
+
+// function calculator(num1, num2, operator) {
+    // return operator(num1, num2);
+// }
+
+// calculator(4, 5, multiply);
+// calculator(4, 5, add);
+
+// debugger;
+// shift enter
+// calculator(4, 4, add);
