ff3dccf33277bba25342e796b5c19fb8e97bade9
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-06-15 19:24:13 +0200

    drum kit project changes

--- 18 Advanced Javascript and DOM Manipulation/Drum Kit Project/index.js
+++ 18 Advanced Javascript and DOM Manipulation/Drum Kit Project/index.js
@@ -42,7 +42,60 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
                 kick.play();
                 break;
 
-            default:
+            default: console.log(buttonInnerHTML);
+                break;
+        }
+
+    });
+
+}
+
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+        var buttonInnerHTML = this.innerHTML;
+
+        // switch statement
+        switch (buttonInnerHTML) {
+            case "w":
+                var tom1 = new Audio("./sounds/tom-1.mp3");
+                tom1.play();
+                break;
+
+            case "a":
+                var tom2 = new Audio("./sounds/tom-2.mp3");
+                tom2.play();
+                break;
+
+            case "s":
+                var tom3 = new Audio("./sounds/tom-3.mp3");
+                tom3.play();
+                break;
+
+            case "d":
+                var tom4 = new Audio("./sounds/tom-4.mp3");
+                tom4.play();
+                break;
+
+            case "j":
+                var snare = new Audio("./sounds/snare.mp3");
+                snare.play();
+                break;
+
+            case "k":
+                var crash = new Audio("./sounds/crash.mp3");
+                crash.play();
+                break;
+
+            case "l":
+                var kick = new Audio("./sounds/kick-bass.mp3");
+                kick.play();
+                break;
+
+            default: console.log(buttonInnerHTML);
                 break;
         }
 
@@ -52,7 +105,56 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
 
 
+addEventListener("keydown", function ()  {
+    alert("Key was pressed");
+})
 
+addEventListener("keydown", function (event) {
+    alert("Key was pressed");
+    console.log(event);
+})
+
+KeyboardEvent.key
+
+
+// Methods Metoda to funkcja która jest powiązana z obiektem
+
+// function moveSuitcase {
+    // alert("May U take your suitcase?");
+    // pickUpSuitcase();
+    // moveSuitcase();
+// }
+
+// var bellBoy1 = {
+    // name: "Tim",
+    // age: 19,
+    // hasWorkPermit: true,
+    // languages: ["Polish", "English"],
+    // moveSuitcase: function () {
+        // alert("May I take yur suitcase?");
+        // picUpSuitcase();
+        // move();
+    // }
+// }
+
+// Constructor Function: nazwa z duzej litery po tym poznajemy ze to funcja konstruktor
+
+// function BellBoy(name, age, hasWorkPermit, languages) {
+    // this.name = name;
+    // this.age = age;
+    // this.hasWorkPermit = hasWorkPermit;
+    // this.languages = languages;
+    // this.moveSuitcase = function () {
+        // alert("May I take your suitcase?");
+        // picUpSuitcase();
+        // move();
+    // }
+// }
+
+// Odwołanie do metody(call Method)
+
+// bellBoy1.moveSuitcase(); (jedyną różnicą miedzy metodą a właśnością (property) są nawiasy)
+// -------------------------------------------------------------------------------------------------
 // for (var i = 0; i < numberOfDrumButtons; i++) {
     // document.querySelectorAll(".drum")[i].addEventListener("click", playDrum);
 // }
