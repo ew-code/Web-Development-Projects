189e11024b56c4fdddd38bb0a972dccae33d11f5
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-06-18 11:29:41 +0200

    Drum Kit makeSound function for switch statement

--- 18 Advanced Javascript and DOM Manipulation/Drum Kit Project/index.js
+++ 18 Advanced Javascript and DOM Manipulation/Drum Kit Project/index.js
@@ -1,3 +1,6 @@
+
+//Detecting Button Press
+
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
@@ -5,105 +8,55 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
         var buttonInnerHTML = this.innerHTML;
 
-        // switch statement
-        switch (buttonInnerHTML) {
-            case "w":
-                var tom1 = new Audio("./sounds/tom-1.mp3");
-                tom1.play();
-                break;
-
-            case "a":
-                var tom2 = new Audio("./sounds/tom-2.mp3");
-                tom2.play();
-                break;
-
-            case "s":
-                var tom3 = new Audio("./sounds/tom-3.mp3");
-                tom3.play();
-                break;
-
-            case "d":
-                var tom4 = new Audio("./sounds/tom-4.mp3");
-                tom4.play();
-                break;
-
-            case "j":
-                var snare = new Audio("./sounds/snare.mp3");
-                snare.play();
-                break;
-
-            case "k":
-                var crash = new Audio("./sounds/crash.mp3");
-                crash.play();
-                break;
-
-            case "l":
-                var kick = new Audio("./sounds/kick-bass.mp3");
-                kick.play();
-                break;
-
-            default: console.log(buttonInnerHTML);
-                break;
-        }
-
+        makeSound(buttonInnerHTML);
     });
 
 }
 
-// document.addEventListener("keydown", function (event) {
-    // alert("Key was pressed");
-    // console.log(event);
-// })
-// 
-window.addEventListener(
-    "keydown",
-    (event) => {
-        if (event.defaultPrevented) {
-            return; // Do nothing if the event was already processed
-        }
+//Detecting Keyboard Press
 
-        switch (event.key) {
-            case "w":
-                var tom1 = new Audio("./sounds/tom-1.mp3");
-                tom1.play();
-                break;
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key);
 
-            case "a":
-                var tom2 = new Audio("./sounds/tom-2.mp3");
-                tom2.play();
-                break;
+})
 
-            case "s":
-                var tom3 = new Audio("./sounds/tom-3.mp3");
-                tom3.play();
-                break;
-            case "d":
-                var tom4 = new Audio("./sounds/tom-4.mp3");
-                tom4.play();
-                break;
+function makeSound(key) {
 
-            case "j":
-                var snare = new Audio("./sounds/snare.mp3");
-                snare.play();
-                break;
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var snare = new Audio("./sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            var kick = new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        default: console.log(buttonInnerHTML);
+            break;
+    }
+}
 
-            case "k":
-                var crash = new Audio("./sounds/crash.mp3");
-                crash.play();
-                break;
-
-            case "l":
-                var kick = new Audio("./sounds/kick-bass.mp3");
-                kick.play();
-                break;
-
-            default: alert("Press another key");
-                return;
-        }
-    },
-    true
-);
-
+//notatki z wykładów ->
 
 // addEventListener("keydown", function ()  {
 // alert("Key was pressed");
