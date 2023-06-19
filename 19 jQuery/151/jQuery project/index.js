8d1aa2039be53eb95b058cf099496fb2941b8557
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-06-19 19:24:47 +0200

    jquery changes

--- 19 jQuery/151/jQuery project/index.js
+++ 19 jQuery/151/jQuery project/index.js
@@ -1,6 +1,10 @@
-$("h1").addClass("big-title margin-50");
+// $("h1").addClass("big-title margin-50"); //adding classes to element in jQuery
 // $("h1").removeClass("big-title");
 
-$("h1").hassClass("big-title margin-50");//true false
+// $("h1").hassClass("big-title margin-50");//true false //sprawdzenie czy element daną klasę posiada
 
-$("button");
+// $("button");
+
+$("h1").text("Bye"); //zmiana tekstu
+
+$("button").text("Don't Click Me");//wybiera wszytkie przyciski na stronie i zmienia ich tekst
