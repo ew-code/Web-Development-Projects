c2b8f464ab06f4e5de024a5c0af3307c7ec62081
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-06-22 19:45:20 +0200

    simon game canges

--- 20 The Simon Game/game.js
+++ 20 The Simon Game/game.js
@@ -24,3 +24,35 @@ $("#red").click(function () {
 $("#green").click(function () {
     $(this).fadeOut(100).fadeIn(100);
 });
+
+$("button").on("click", function () {
+    $("h1").slideUp().slideDown().animate({ opacity: 0.5 });
+});
+
+function makeSound(button) {
+
+    switch (button) {
+        case "blue":
+            var blue = new Audio("./sounds/blue.mp3");
+            blue.play();
+            break;
+        case "green":
+            var green = new Audio("./sounds/green.mp3");
+            green.play();
+            break;
+        case "red":
+            var red = new Audio("./sounds/red.mp3");
+            red.play();
+            break;
+        case "yellow":
+            var yellow = new Audio("./sounds/yellow.mp3");
+            yellow.play();
+            break;
+        case "wrong":
+            var wrong = new Audio("./sounds/wrong.mp3");
+            wrong.play();
+            break;
+        default: console.log(buttonInnerHTML);
+            break;
+    }
+}
