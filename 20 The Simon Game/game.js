54a6bb03b40e837d3256a99385b8991e9b959090
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-06-22 17:54:08 +0200

    buttonColours and randomNumber added to Simon Game

--- 20 The Simon Game/game.js
+++ 20 The Simon Game/game.js
@@ -1,1 +1,8 @@
-alert("wow");
+const buttonColours = ["red", "blue", "green", "yellow"];
+
+let randomNumber
+
+function nextSequence() {
+    // let randomNumber = $.random(0, 3);
+    randomNumber = Math.floor(Math.random() * 3) + 1;
+}
