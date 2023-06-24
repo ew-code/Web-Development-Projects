2bb9be15b820c19e6b10de551b6aaaf76a7a34dd
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-06-24 22:28:26 +0200

    Format code

--- 20 The Simon Game/game.js
+++ 20 The Simon Game/game.js
@@ -6,46 +6,46 @@ let gameStarted = false;
 let level = 0;
 
 $(document).one("keypress", function () {
-    if (!gameStarted) {
-        $("#level-title").text("Level " + level);
-        nextSequence().delay(800);
-        gameStarted = true;
-    }
+  if (!gameStarted) {
+    $("#level-title").text("Level " + level);
+    nextSequence().delay(800);
+    gameStarted = true;
+  }
 });
 
 function nextSequence() {
-    $("#level-title").text("Level " + level);
-    level++;
-    let randomNumber = Math.floor(Math.random() * 4);
-    let randomChosenColour = buttonColours[randomNumber];
-    gamePattern.push(randomChosenColour);
+  $("#level-title").text("Level " + level);
+  level++;
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
 
-    $("#" + randomChosenColour)
-        .fadeIn(100)
-        .fadeOut(100)
-        .fadeIn(100);
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
 
-    playSound(randomChosenColour);
+  playSound(randomChosenColour);
 }
 
 $(".btn").on("click", function () {
-    let userChosenColour = $(this).attr("id");
-    userClickedPattern.push(userChosenColour);
-    playSound(userChosenColour);
-    animatePress(userChosenColour);
+  let userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
 
-    $("#level-title").text("Level " + level);
-    nextSequence().delay(800);
+  $("#level-title").text("Level " + level);
+  nextSequence().delay(800);
 });
 
 function playSound(soundFile) {
-    var audio = new Audio("./sounds/" + soundFile + ".mp3");;
-    audio.play();
+  var audio = new Audio("./sounds/" + soundFile + ".mp3");
+  audio.play();
 }
 
 function animatePress(currentColour) {
-    $("#" + currentColour).addClass("pressed");
-    setTimeout(function () {
-        $("#" + currentColour).removeClass("pressed");
-    }, 100);
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
 }
