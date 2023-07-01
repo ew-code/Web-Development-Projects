f033f9388fb5d12f488aaa9569e401caab3a0e02
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-07-01 12:15:20 +0200

    Unix Command line notes

---
+++ 21 Unix Command Line/index.js
@@ -0,0 +1,42 @@
+mkdir - make directory(folder);
+
+ls - list(pokazuje liste folderów)
+
+cd - (change directory) i wpisujemy nazwę folderu do którego chcemy się dostać.
+
+    tab(klawisz_ podpowiada)
+
+cd ~  - wróć do root directory
+
+ostatnia komenda--strzałka w górę
+
+cd..  - back one level
+
+alt click - przeniesie kursor
+
+ctrl a - start na początek lini
+ctrl e - end na koniec lini
+
+ctrl u - clear terminal
+
+touch - tworzy nowy plik 
+
+start Text2.txt(otworzy plik tekstowy)
+
+code - (Open in vs code)
+
+rm - Text1.rtf(remove files)
+
+rm * - usunie wszytkie pliki z folderu
+
+pwd(print workin directory) - podpowiada gdzie jesteś aktualnie
+
+rm - r(remove directory - usunie całą zawartość folderuu)
+
+sudo(super user do - używa admina i jego uprawnień)
+
+sudo rm - rf(usunie force czyli bez pytania o zgodę)
+
+greap 
+
+curl 
