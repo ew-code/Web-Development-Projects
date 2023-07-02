e5c9fae916b578fa51c3b458c25bb74a1ff3a788
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-07-02 12:17:02 +0200

    NodeJs changes read from file added

--- 23_NODE.js/2.2 Native Modules/index.js
+++ 23_NODE.js/2.2 Native Modules/index.js
@@ -1,11 +1,18 @@
 const fs = require('fs');
 
 // const data = new Uint8Array(Buffer.from('Hello Node.js'));
-fs.writeFile('message2.txt', "Hello from NodeJs!", (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
-}); 
+// fs.writeFile('message2.txt', "Hello from NodeJs!", (err) => {
+    // if (err) throw err;
+    // console.log('The file has been saved!');
+// });
 
+
+fs.readFile('./message2.txt','utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+});
 
 // fs.writeFile(file, data[, options], callback)
 // https://nodejs.org/dist/latest-v20.x/docs/api/fs.html#fswritefilefile-data-options-callback
+
+// fs.readFile(path[, options], callback)
