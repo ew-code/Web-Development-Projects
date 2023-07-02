4b6b1dd710e7593395ea90a4169dc9704a545a33
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-07-02 13:51:24 +0200

    NPM superheroes package added

--- 23_NODE.js/2.3 NPM/index.js
+++ 23_NODE.js/2.3 NPM/index.js
@@ -1,7 +1,13 @@
-var generateName = require('sillyname');
-var sillyName = generateName();
+// var generateName = require('sillyname');
+import generateName from "sillyname";
+import superheroes from "superheroes";
 
-console.log('My name is ' + sillyName );
+var sillyName = generateName();
+var superheroName = superheroes.random();
+
+console.log('My name is ' + sillyName);
+console.log('My name is ' + superheroName);
 
 // 1) npm init
 // 2) npm instal
+// 3) npm i module
