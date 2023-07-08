6dd9bbb61eaa441cef0274b71fd40e5396897bc1
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-07-08 12:21:10 +0200

    calculator.js added

---
+++ 24_Express.js_with_Node.js/Calculator/calculator.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get("/", function (req, res) {
+    res.send("Hello Calclator");
+});
+
+app.listen(3000, function () {
+    console.log("Server startet on port " + port);
+});
