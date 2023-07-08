1943a12fbecd21f1a2091bb612a0c87a7d9ec4dc
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-07-08 12:44:13 +0200

    server.js changes

--- 24_Express.js_with_Node.js/my-express-server/server.js
+++ 24_Express.js_with_Node.js/my-express-server/server.js
@@ -6,6 +6,14 @@ app.get("/", function (req, res) {
     res.send("<h1>Hello Express.js!</h1>");
 });
 
+app.get("/contact", function (req, res) {
+    res.send("Contact me at: eweli.wes@gmail.com");
+});
+
+app.get("/about", function (req, res) {
+    res.send("about me : https://github.com/ew-code");
+});
+
 app.listen(3000, function(){
     console.log("Server startet on port " + port);
 });
