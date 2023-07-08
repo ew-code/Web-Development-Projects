28af715be91b4b9d761d2bcd6db1aa798f9b0ae0
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-07-08 10:23:39 +0200

    server.js changes

--- 24_Express.js_with_Node.js/my-express-server/server.js
+++ 24_Express.js_with_Node.js/my-express-server/server.js
@@ -2,10 +2,26 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+// app.get('/', (req, res) => {
+    // res.send('Hello World!');
+// })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+// app.listen(port, () => {
+    // console.log(`Example app listening on port ${port}`);
+// });
+
+app.get("/", function (request, response) {
+    console.log(request);
 });
+
+app.listen(3000, function(){
+    console.log("Server startet on port ${port}");
+});
+
+
+
+
+// node server.js (to start)
+// ctrl + C (stop)
+
+// https://expressjs.com/en/starter/hello-world.html
