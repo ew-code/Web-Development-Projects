87c8272cc080130a1a62c2822dd5a9bb428bb886
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-07-09 12:41:52 +0200

    calculator changes

--- 24_Express.js_with_Node.js/Calculator/calculator.js
+++ 24_Express.js_with_Node.js/Calculator/calculator.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function (req, res) {
+
+    console.log(req.body);
     res.send("Tkank you for posting that");
 });
 
