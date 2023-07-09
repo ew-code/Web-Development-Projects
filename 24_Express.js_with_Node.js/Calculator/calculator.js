ce7a92f61ec3f2f6fa133d4c9204d1d4203fbdd7
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-07-09 12:02:58 +0200

    calculator changes

--- 24_Express.js_with_Node.js/Calculator/calculator.js
+++ 24_Express.js_with_Node.js/Calculator/calculator.js
@@ -6,6 +6,10 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/", function (req, res) {
+    res.send("Tkank you for posting that");
+});
+
 app.listen(3000, function () {
     console.log("Server is running on port " + port);
 });
