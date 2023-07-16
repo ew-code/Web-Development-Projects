608f0c1885c39015bd6ed30ce344e3d642b1ee8f
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-07-16 10:08:36 +0200

    WeatherProject changes in app.js

---
+++ 25_API-Application Programming Interfaces/WeatherProject/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const https = require("https");
+
+const app = express();
+const port = 3000;
+
+//make get request to external server node -> https://www.twilio.com/blog/http-requests-in-node-js.html
+//https://nodejs.org/api/https.html -> https://nodejs.org/api/https.html#httpsgeturl-options-callback
+app.get("/", function (req, res) {
+    const url = "https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=ad8e5204b70c5930d2d7d2b9298450f6&q=Warsaw&units=metric";
+
+    https.get(url, function (response) {
+        console.log(response);
+    })
+    
+    res.send("Server is up and running");
+});
+
+
+
+
+
+app.listen(3000, function () {
+    console.log("Server is running on port " + port);
+});
