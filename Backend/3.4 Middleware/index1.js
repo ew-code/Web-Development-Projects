6ac41a25a448f464f00a6419a682bd7aac10b3a9
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-07-20 19:23:45 +0200

    app.use added

--- Backend/3.4 Middleware/index1.js
+++ Backend/3.4 Middleware/index1.js
@@ -2,6 +2,7 @@ import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
@@ -9,6 +10,8 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
