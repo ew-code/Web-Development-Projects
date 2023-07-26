70f57acd1f736c0b1135bd81d66349dff6209cf1
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-07-26 19:41:19 +0200

    index.js changes

--- Backend/4.0_EJS/index.js
+++ Backend/4.0_EJS/index.js
@@ -9,18 +9,14 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-let userIsAuthorised = false;
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
-function passwordCheck(req, res, next) {
+function dayCheck(req, res, next) {
+    const d = new Date();
+    let day = d.getDay();
 
-    // const d = new Date();
-// let day = d.getDay();
-//
-// console.log(day);
-    
-    
+    console.log(day);
+
     // const password = req.body["password"];
     // if (password === "ILoveProgramming") {
         // userIsAuthorised = true;
@@ -29,18 +25,17 @@ function passwordCheck(req, res, next) {
 }
 
 
-app.use(passwordCheck);
+app.use(dayCheck);
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
+    res.send(__dirname + "/public/index.html");
 });
 
-app.post("/check", (req, res) => {
-    if (userIsAuthorised) {
+app.post("/", (req, res) => {
+    if (week) {
         res.sendFile(__dirname + "/public/secret.html");
     } else {
         res.sendFile(__dirname + "/public/index.html");
-        //Alternatively res.redirect("/");
     }
 });
 
