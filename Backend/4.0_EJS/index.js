a5691d5b383cf8f6b5fb1329b376ba796c7afbfc
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-07-29 19:44:49 +0200

    EJS changes in index.ejs and index.js

--- Backend/4.0_EJS/index.js
+++ Backend/4.0_EJS/index.js
@@ -1,42 +1,41 @@
 import express from "express";
 import bodyParser from "body-parser";
 
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
-function dayCheck(req, res, next) {
-    // const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-    const weekend = ["Sunday", "Saturday"];
-
-    const d = new Date();
-    let day = week[d.getDay()];
-
-    if (week.includes(day)) {
-        res.send("Hey! It's a weekday, it's time to work hard!");
-    } else if (weekend.includes(day)) {
-        res.send("Hey! It's the weekend, it's time to have fun!");
-    }
-    next();
-}
-
-app.use(dayCheck);
-
 app.get("/", (req, res) => {
-    res.render(__dirname + "/views/index.ejs");
+    res.render("index.ejs", { dayType: "a weekday", advice: "it's time to work hard" });
 });
 
-app.post("/", (req, res) => {
-    res.render(__dirname + "/views/index.ejs",
-        { name: req.body["dayCheck"] });
-});
+
+
+// function dayCheck(req, res, next) {
+    // const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    // const week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+    // const weekend = ["Sunday", "Saturday"];
+// 
+    // const d = new Date();
+    // let day = week[d.getDay()];
+// 
+    // if (week.includes(day)) {
+        // res.send("Hey! It's a weekday, it's time to work hard!");
+    // } else if (weekend.includes(day)) {
+        // res.send("Hey! It's the weekend, it's time to have fun!");
+    // }
+    // next();
+// }
+// 
+// app.use(dayCheck);
+// 
+
+
+
+
+// app.get("/", (req, res) => {
+    // res.render(__dirname + "/views/index.ejs",
+        // { name: req.body["dayCheck"] });
+// });
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
