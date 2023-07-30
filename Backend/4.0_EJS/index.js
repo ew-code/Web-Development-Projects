b499519867c60005f434ad78758d450b4041263e
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-07-30 09:24:58 +0200

    index.js changes

--- Backend/4.0_EJS/index.js
+++ Backend/4.0_EJS/index.js
@@ -10,6 +10,11 @@ app.get("/", (req, res) => {
     let type = "a weekday";
     let adv = "it's time to work hard";
 
+    if (day === 0 || day === 6) {
+        type = "the weekend";
+        adv = "it's time to have some fun";
+    }
+
 res.render("index.ejs", {
                 dayType: "a weekday",
                 advice: "it's time to work hard"
