7ca4ffd0fff69be838d810691b8a7418248c24f2
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-08-27 12:16:40 +0200

    To do App changes

--- Backend/4.5 To do list Application/index.js
+++ Backend/4.5 To do list Application/index.js
@@ -11,15 +11,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
     const fullDate = dateFormat("fullDate");
 
-
     res.render("index.ejs", { date : fullDate});
     console.log(fullDate);
 });
 
-// app.post("/submit", (req, res) => 
-    // 
-    // res.render("index.ejs", { adjective: randomAdj, noun: randomNoun });
-// });
+app.post("/", (req, res) => {
+    let newItem = req.body.newItem;
+    res.render("index.ejs", { newListItem: newItem });
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
