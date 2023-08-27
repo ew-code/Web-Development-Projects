d65f851937374272336701963538fbb22a91640f
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-08-27 09:55:47 +0200

    To do list App changes fullDate added

--- Backend/4.5 To do list Application/index.js
+++ Backend/4.5 To do list Application/index.js
@@ -1,21 +1,26 @@
 import express from "express";
 import bodyParser from "body-parser";
+import dateFormat from "dateformat";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
-
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(dateFormat);
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    const fullDate = dateFormat("fullDate");
+
+
+    // res.render("index.ejs");
+    console.log(fullDate);
 });
 
-app.post("/submit", (req, res) => 
-    
-    res.render("index.ejs", { adjective: randomAdj, noun: randomNoun });
-});
+// app.post("/submit", (req, res) => 
+    // 
+    // res.render("index.ejs", { adjective: randomAdj, noun: randomNoun });
+// });
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
