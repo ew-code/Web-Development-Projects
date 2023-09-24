53b2bf23485275cc3d1eaf07908c152ba027859c
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-09-24 13:39:46 +0200

    Axios changes

--- Backend/5.3 Axios/index.js
+++ Backend/5.3 Axios/index.js
@@ -5,6 +5,7 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
